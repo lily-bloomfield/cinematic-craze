9376bcc7ce34e0b241f4417692c5ae9c2b9b7d9f
lily-bloomfield <lilybloomfield.lb@gmail.com>
2024-05-14 18:08:12 +0630

    detail-pg

--- script.js
+++ script.js
@@ -1,48 +1,71 @@
-const moviesData = [
-  {
-    id: 1,
-    name: "The Idea of You",
-    duration: "116m",
-    genre: ["Romance", "Comedy", "Music"],
-    countryOrigin: "",
-    imgUrl:
-      "https://img.myflixerz.to/xxrz/250x400/201/da/8b/da8bd47bd5ebd4d4460303d30af5e738/da8bd47bd5ebd4d4460303d30af5e738.jpg",
-    imdbRating: 6.5,
-    type: "movie",
-    trailer: "url",
-    releaseDate: [16, 3, 2024], //day, month, yr
-  },
-];
+document.addEventListener("DOMContentLoaded", function () {
+  const apiKey = `6d1c6613688dfb822a0100f7a702697b`;
+  // trending movies & series (all)
+  const urlTrending = "https://api.themoviedb.org/3/trending/all/day?";
+  //latest movies $ series
+  const urlLatestTv = `https://api.themoviedb.org/3/discover/tv?`;
+  const urlLatestMovies = `https://api.themoviedb.org/3/discover/movie?`;
+  //comming soon
+  const urlUpcoming = `https://api.themoviedb.org/3/movie/upcoming?`;
 
-const trendingMoviesContainer = document.querySelector(
-  ".trending-movies-container"
-);
-const latestMoviesContainer = document.querySelector(
-  ".latest-movies-container"
-);
-const displayMovies = (para) => {
-  for (let i = 0; i < 24; i++) {
-    const trendingMovies = document.createElement("div");
-    const movieImg = document.createElement("img");
-    const movieTitle = document.createElement("h4");
-    const movieInfo = document.createElement("p");
-    const playBtn = document.createElement("i");
+  const bodyContainer = document.querySelector("body");
+  const trendingMoviesContainer = document.querySelector(
+    ".trending-movies-container"
+  );
+  const latestSeriesContainer = document.querySelector(
+    ".latest-movies-container"
+  );
+  const upcomingContainer = document.querySelector(
+    ".upcoming-movies-container"
+  );
+  const movieDatas = async (urlPara, para) => {
+    const url = `${urlPara}api_key=${apiKey}`;
+    const data = await fetch(url);
+    const dataFromServer = await data.json();
 
-    movieImg.classList.add("movie-img");
-    movieInfo.classList.add("trending-movies-p");
-    trendingMovies.classList.add("trending-movies-div");
-    playBtn.classList.add("fa-solid", "fa-circle-play");
-    trendingMovies.append(movieImg, playBtn, movieTitle, movieInfo);
-    if (para === "default") {
-      trendingMoviesContainer.append(trendingMovies);
-    } else if (para === "latest") {
-      latestMoviesContainer.append(trendingMovies);
-    }
+    console.log(dataFromServer.results);
+    const movieData = dataFromServer.results;
+    movieData.map((src) => {
+      const imgPoster = src.backdrop_path;
+      const movieName = src.original_title || src.original_name;
+      // console.log(imgPoster, movieName);
 
-    movieImg.setAttribute("src", moviesData[0].imgUrl);
-    movieTitle.textContent = moviesData[0].name;
-    movieInfo.textContent = `2024  |  ${moviesData[0].duration}`;
-  }
-};
-displayMovies("default");
-displayMovies("latest");
+      const trendingMovies = document.createElement("div");
+      const movieImg = document.createElement("img");
+      const movieTitle = document.createElement("h4");
+      const movieInfo = document.createElement("p");
+      const playBtn = document.createElement("i");
+
+      movieImg.classList.add("movie-img");
+      movieInfo.classList.add("trending-movies-p");
+      trendingMovies.classList.add("trending-movies-div");
+      playBtn.classList.add("fa-solid", "fa-circle-play");
+      trendingMovies.append(movieImg, playBtn, movieTitle, movieInfo);
+      if (para === "trendingMovies") {
+        trendingMoviesContainer.append(trendingMovies);
+      } else if (para === "latest") {
+        latestSeriesContainer.append(trendingMovies);
+      } else if (para === "upcoming") {
+        upcomingContainer.append(trendingMovies);
+      }
+      // trendingMoviesContainer.append(trendingMovies);
+
+      movieImg.setAttribute(
+        "src",
+        `https://image.tmdb.org/t/p/w500${imgPoster}`
+      );
+      movieTitle.textContent = movieName;
+      movieInfo.textContent = `2024  |  116m`;
+
+      //clicked movie
+      trendingMovies.addEventListener("click", (event) => {
+        event.preventDefault();
+        console.log("clicked");
+        window.location.href = "detail.html";
+      });
+    });
+  };
+  movieDatas(urlTrending, "trendingMovies");
+  movieDatas(urlLatestTv, "latest");
+  movieDatas(urlUpcoming, "upcoming");
+});
